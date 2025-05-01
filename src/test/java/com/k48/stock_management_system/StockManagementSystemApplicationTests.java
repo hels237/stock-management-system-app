d1483bc7842c228bb6c9e91fbf60314325d94298
hels <m7hels@gmail.com>
2025-05-01 14:18:35 +0100

    add  some started new files on the project

---
+++ src/test/java/com/k48/stock_management_system/StockManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k48.stock_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
